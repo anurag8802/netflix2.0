e8de3a7daff21f6fded2b11d9f318b30ec4c929a
anurag <jhaanurag407@gmail.com>
2024-06-02 11:11:03 +0530

    changes

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ dotenv.config({
     path:".env"
 })
 
-const BASE_URL = process.env.BASE_URL;
 
 databaseConnection();
 
@@ -18,11 +17,35 @@ const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
-const corsOptions = {
-    origin:'http://localhost:3000',
-    credentials:true
-}
+// const corsOptions = {
+//     origin:'http://localhost:3000/' ,
+//     credentials:true,
+//     "secure": false,
+//     "changeOrigin": true,
+//     optionSuccessStatus:200
+// }
+
 app.use(cors(corsOptions));
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://nimble-pony-61673b.netlify.app'
+  ];
+  
+  app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }
+  }));
+  
+  // Your routes here
+  
+  app.listen(3000, () => {
+    console.log('Server running on port 3000');
+  });
 
 
 app.use("/api/v1/user", userRoute);
