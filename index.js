cf99235acdad8cefda3fae42e5f8b6f92e2554bc
anurag <jhaanurag407@gmail.com>
2024-06-02 11:29:27 +0530

    changes

--- index.js
+++ index.js
@@ -17,31 +17,15 @@ const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
-// const corsOptions = {
-//     origin:'http://localhost:3000/' ,
-//     credentials:true,
-//     "secure": false,
-//     "changeOrigin": true,
-//     optionSuccessStatus:200
-// }
+const corsOptions = {
+    origin:'http://localhost:3000/' ,
+    credentials:true,
+    "secure": false,
+    "changeOrigin": true,
+    optionSuccessStatus:200
+}
 
-// app.use(cors(corsOptions));
-const allowedOrigins = [
-    'http://localhost:3000',
-    'https://nimble-pony-61673b.netlify.app'
-  ];
-  
-  app.use(cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    }
-  }));
-
-app.use("/api/v1/user", userRoute);
+app.use(cors(corsOptions));
 
 
 app.listen(process.env.PORT,() => {
