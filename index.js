61dec55ddfab66fca4f30bb802f5c9ed5e5e68cb
anurag <jhaanurag407@gmail.com>
2024-06-04 13:11:00 +0530

    changes

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://stately-semolina-9271b8.netlify.app/',
+    origin:'http://localhost:3000',
     credentials:true
 }
 app.use(cors(corsOptions));
